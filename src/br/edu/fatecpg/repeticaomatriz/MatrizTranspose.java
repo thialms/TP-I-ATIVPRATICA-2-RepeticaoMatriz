09c21474e34e4f3007ae829da3e8cb1202739d3b
Thiago de Almeida Souza Silva <thiiago.allmeida44@gmail.com>
2025-02-27 08:28:59 -0300

    Update MatrizTranspose.java
    fix: MatrizTranspose working correctly

--- src/br/edu/fatecpg/repeticaomatriz/MatrizTranspose.java
+++ src/br/edu/fatecpg/repeticaomatriz/MatrizTranspose.java
@@ -5,25 +5,32 @@ import java.util.Scanner;
 public class MatrizTranspose {
 
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		int matriz[][] = new int[3][3];
-		
-		for (int i=0;i<matriz.length;i++) {
-			System.out.println("Digite os valores para a " + (i+1) + " linha:");
-			for (int j=0;j<3;j++) {
-				matriz[i][j] = scanner.nextInt();
-			}
-		}
-		
-		for (int i=0;i<matriz.length;i++) {
-			for (int j=0;j<matriz.length;j++) {
-				System.out.print(matriz[j][i] + ",");
-			}
-			System.out.println();
-		}
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int matriz1[][] = new int[3][2];
+        int matriz2[][] = new int[2][3];
+        
 
-	}
+        for (int i = 0; i < matriz1.length; i++) {
+            System.out.println("Digite os valores para a " + (i + 1) + "ª linha:");
+            for (int j = 0; j < matriz1[i].length; j++) { 
+                matriz1[i][j] = scanner.nextInt();
+            }
+        }
+        
 
+        for (int i = 0; i < matriz1.length; i++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
+                matriz2[j][i] = matriz1[i][j]; 
+            }
+        }
+
+        for (int i = 0; i < matriz2.length; i++) {
+            for (int j = 0; j < matriz2[i].length; j++) {
+                System.out.print(matriz2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
